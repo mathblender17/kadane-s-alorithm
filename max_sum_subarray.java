0c6b78735feeafccbe129742d7f3fefe60d34169
mathblender17 <114827353+mathblender17@users.noreply.github.com>
2023-02-07 21:39:21 +0530

    Update max_sum_subarray.java

--- max_sum_subarray.java
+++ max_sum_subarray.java
@@ -1,10 +1,10 @@
 import java.util.*;
 import java.lang.*;
 public class max_sum_subarray
-{
-    public static void kadanes(int num[])//we use the kadanes algorithm as it has good time complexity
+{   //we use the kadanes algorithm as it has O(n) time complexity
+    public static void kadanes(int num[])
     {
-        int ms=Integer.MIN_VALUE;
+        int ms=Integer.MIN_VALUE;  //max sum is set to -infinity
         int cs=0;
 
         for(int i=0;i<num.length;i++)
@@ -18,6 +18,7 @@ public class max_sum_subarray
         System.out.println("Our max subarray sum is : "+ ms);
 
     }
+    //This is true only when all the elements of the array are greater than zero
     public static void main(String args[])
     {
         int num[]={-2, -3, 4, -1, -2, 1,5, -3};
